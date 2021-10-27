15255fef179f6822a19f3f65c46b82e0d01b704e
YuanfengYan <yanyuanfeng_sspu@163.com>
2021-10-27 11:14:41 +0800

    登录

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import VueRouter from "./router/index.js"
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 import './mock'
 
-createApp(App).use(VueRouter).mount('#app')
+createApp(App).use(VueRouter).use(ElementPlus).mount('#app')
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,20 +3,72 @@
  * @Author: yanyuanfeng
  * @Date: 2021-10-26 17:11:07
  * @LastEditors: yanyuanfeng
- * @LastEditTime: 2021-10-26 17:14:20
+ * @LastEditTime: 2021-10-27 11:11:11
 -->
 <template>
-  <div>login - {{name}}</div>
+<el-form
+    ref="ruleForm"
+    :model="ruleForm"
+    status-icon
+    :rules="rules"
+    label-width="120px"
+    class="demo-ruleForm"
+  >
+    <el-form-item label="用户名" prop="name">
+      <el-input v-model="ruleForm.name"></el-input>
+    </el-form-item>
+    <el-form-item>
+    <el-form-item label="密码" prop="pass">
+      <el-input
+        v-model="ruleForm.pass"
+        type="password"
+        autocomplete="off"
+      ></el-input>
+    </el-form-item>
+      <el-button type="primary" @click="submitForm('ruleForm')"
+        >登录</el-button
+      >
+    </el-form-item>
+  </el-form>
 </template>
 <style scoped>
-
 </style>
-<script>
+<script >
+import {loginApi} from '@/api/user'
 export default {
-  data(){
-    return {
-      name:'xxx'
+  data() {
+    
+    const validateEmpty = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input '))
+      } else {
+        callback()
+      }
     }
-  }
+    return {
+      ruleForm: {
+        pass: '',
+        name: '',
+      },
+      rules: {
+        name: [{ validator: validateEmpty, trigger: 'blur' }],
+        pass: [{ validator: validateEmpty, trigger: 'blur' }],
+      },
+    }
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+         loginApi(valid).then(res=>{
+            console.log('loginApi',res)
+          })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  },
 }
 </script>
